586eed0c80d215a3bf8d9ceaab329fc346b5d78b
joseph0x45 <zozozozeph@gmail.com>
2025-05-24 12:12:33 +0000

    feat: say sorry

--- handlers.js
+++ handlers.js
@@ -2,4 +2,8 @@ function hello_handler(){
   return "world\n"
 }
 
+func sorry_handler(){
+  return "sorry\n"
+}
+
 module.exports = { hello_handler }
--- handlers.test.js
+++ handlers.test.js
@@ -3,3 +3,7 @@ const { hello_handler } = require('./handlers');
 test('hello_handler returns "world\\n"', () => {
   expect(hello_handler()).toBe("world\n");
 });
+
+test('sorry_handler returns "world\\n"', () => {
+  expect(sorry_handler()).toBe("world\n");
+});
